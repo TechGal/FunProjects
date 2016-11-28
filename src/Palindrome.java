d96b9949f0989a9607dc10116b583a9805f62d17
Aila <aila@simpsonirving.com>
2016-11-27 18:36:09 -0800

    Fixed a small bug

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main (String[] args) {
         Scanner user_input = new Scanner(System.in);
+        boolean found = false;
 
-        System.out.println("What's your name?");
-        String name = user_input.next();
-        System.out.println("Hello " + name);
-        System.out.println("Enter a palindrome");
-        String word = user_input.next();
-        word = word.toLowerCase();
-        String reverse = new StringBuffer(word).reverse().toString();
-        if (word.equals(reverse)) {
-            System.out.println("You entered a palindrome!");
-        }
-        else {
-            System.out.println(word + " is not a palindrome!");
+            System.out.println("What's your name?");
+            String name = user_input.next();
+            System.out.println("Hello " + name);
+
+            while (found == false) {
+                System.out.println("Enter a palindrome");
+                String word = user_input.next();
+                word = word.toLowerCase();
+                String reverse = new StringBuffer(word).reverse().toString();
+                if (word.equals(reverse)) {
+                    System.out.println("You entered a palindrome!");
+                    found = true;
+                } else {
+                    System.out.println(word + " is not a palindrome!");
+            }
         }
     }
 }
+
